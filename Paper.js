0cdf01725cdc07becef1316708bbed76b9a79c8d
Muskan Kushwaha <73283486+muskankushwaha24680@users.noreply.github.com>
2020-12-08 16:24:25 +0530

    Add files via upload

--- Paper.js
+++ Paper.js
@@ -14,7 +14,7 @@ class Paper{
     this.y = y ;
     this.r = r;
     this.body = Bodies.circle(this.x , this.y , this.r/2 , options);
-    this.image = loadImage("Paper.png");
+    this.image = loadImage("paper.png");
     World.add(world , this.body) ;
 }
 display(){
